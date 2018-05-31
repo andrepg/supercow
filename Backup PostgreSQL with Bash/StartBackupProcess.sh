ba227c4ed9e053bca237e23a404a9fff142eb384
André Paul Grandsire <apg.cadastro@live.com>
2018-05-31 00:02:14 -0300

    Making only one file from two

--- Backup PostgreSQL with Bash/StartBackupProcess.sh
+++ Backup PostgreSQL with Bash/StartBackupProcess.sh
@@ -10,22 +10,45 @@ echo "|         This is our first open source version of bash script "
 echo "|   to make a backup of a PostgreSQL database, zip them and delete"
 echo "|   older files to not fill up hard disk storage. "
 echo "|"
-echo "|         We develop two files that work together. An AppCore.sh file"
-echo "|   and a StartBackupProcess.sh file. The first controls the backup, "
-echo "|   and the second controls all the process, including a call for "
-echo "|   AppCore.sh file. "
 echo "|    "
-echo "|         There's a few variables to configure inside AppCore.sh file, "
-echo "|   but's not that so hard to do. It's a few things like hostname, port, "
-echo "|   username and path where backup will be saved. "
+echo "|         There's a few variables to configure inside this file, "
+echo "|   but's not that so hard to do. It's a few things like hostname,"
+echo "|   server port, username and path where backup will be saved. "
 echo "|    "
 echo "|         It's easy to understand, a lot! "
 echo "|    "
 echo "|    "
 
-sh /backup/AppCore.sh
+current_data=$(date +%Y%m%d)
+hostname="localhost"
+user="alterdata"
+port=5432
+base_path=/backup
+database_array=$(cat $base_path/database_list.txt)
 
-# echo ""
-# echo "- Iniciando sincronizacao com servidor remoto"
-# echo "-- Abrindo conexao rSync"
-# rsync -varzuP --human-readable -e 'ssh -p 22' /backup/* user@host.address.com:/home/backup/
+for sDatabase in $database_array
+do
+	## Gettin our file name (DDMMYYYY-database.sql)
+	## our file path (/backup/some/place/database)
+	## and our log file (/backup/some/place/log/database)
+	file_name=$current_data-$sDatabase.sql
+	file_path=$base_path/$sDatabase
+	log_path=$base_path/log/$sDatabase
+
+	echo "starting backup of database " $sDatabase
+	echo " at path " $file_path
+	echo " and name " $file_name
+
+	## Generate our path if they don't exists
+	mkdir -p $file_path && mkdir -p $log_path
+
+	## Starting our PostgreSQL dumping process (the backup itself) and catching log of this at $log_path
+	pg_dump -i -v -h $hostname -p $port -U $user $sDatabase > $file_path/$file_name 2> $log_path/$current_data.log
+
+	## Also, besides zipping our file, we're deleting our old .sql file that tar process leaves at folder
+	echo "zipping our sql generated file..."
+	cd $file_path && tar -cf $file_name.tar $file_name && xz -z -9 --force $file_name.tar && rm $file_name
+
+	## Deleting files older than 30 days to not consume hard disk storage
+  find /backup/$d  -ctime +15 -exec rm -r {} \;	
+done
