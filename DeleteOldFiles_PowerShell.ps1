4d3d260ce78e8986adc2439d5f38c0ea5b737f85
André Paul Grandsire <andrepg@users.noreply.github.com>
2018-06-30 10:24:34 -0300

    Create DeleteOldFiles_PowerShell.ps1

---
+++ DeleteOldFiles_PowerShell.ps1
@@ -0,0 +1,8 @@
+$limit = (Get-Date).AddDays(-15)
+$path = "C:\Some\Path"
+
+# Delete files older than the $limit.
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+
+# Delete any empty directories left behind after deleting the old files.
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
