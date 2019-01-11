9f405812e2e460359a7a3074d15a1f8bff075a24
André Paul Grandsire <andrepg@users.noreply.github.com>
2019-01-11 12:48:24 -0200

    Improvements to algorithm
    We're improving our code to make more smart operations. With this, now we can check if user's input is blank or provided and check if paths exists.

--- DeleteFiles_ExtensionBased.bat
+++ DeleteFiles_ExtensionBased.bat
@@ -2,10 +2,51 @@
 CLS
 :: Setting variables to be local only and get input from user (for now accepting just one extension per running)
 SETLOCAL EnableExtensions EnableDelayedExpansion
-chcp 65001
+chcp 65001 1>nul
+
 echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+
 SET /p SearchPath=Type path for search..: 
 SET /p ExtensionToSearch=Type the extension that you're searching to [just extension, without dot and asterisk]..: 
+
+IF [%SearchPath%]==[] (
+    echo.
+    echo *
+    echo *                   
+    echo *     ERROR !!!     
+    echo *                   
+    echo *
+    echo *
+    echo *     Search path was not provided. Try again...
+    echo *
+    echo *
+    echo.
+    GOTO:END_SCRIPT
+)
+
+IF [%ExtensionToSearch%]==[] (
+    echo.
+    echo *
+    echo *                   
+    echo *     ERROR !!!     
+    echo *                   
+    echo *
+    echo *
+    echo *     You don't provide an extension to search. Try again...
+    echo *
+    echo *
+    echo.
+    GOTO:END_SCRIPT
+)
 
 IF EXIST %SearchPath% (
     echo.
@@ -14,28 +55,63 @@ IF EXIST %SearchPath% (
 
     echo.
     echo ~ Searching files and saving result in a temporary file
+    echo.
     
-    dir /s /b %SearchPath%\*.%ExtensionToSearch% > "%~dp0temporary_file"
+    dir /s /b %SearchPath%\*.%ExtensionToSearch% 1>"%~dp0temporary_file" 2>nul
+
+    IF !ERRORLEVEL! NEQ 0 (
+        echo.
+        echo *
+        echo *                   
+        echo *     ERROR !!!     
+        echo *                   
+        echo *
+        echo *
+        echo *     No files was found with given parameters ;
+        echo *
+        echo *
+        echo.
+        GOTO:END_SCRIPT
+    )
 
     echo.
     echo ~ Retrieving results and starting to remove files ...
     echo.
 
     FOR /F "usebackq tokens=* eol=#" %%G in ("%~dp0temporary_file") do ( 
-        echo Deleting file ~^> %%G
+        echo        Deleting file ~^> %%G        
+        
         del /f "%%G"
+        SET /A "DeletedFilesCounter+=1"
     )
 
     echo.
+    echo ~ !DeletedFilesCounter! files deleted at this execution
+    echo.
+    echo.
+    echo ~ Removing temporary file created by script
+
+    del /f/q "%~dp0temporary_file"
+
     echo ~ Changing back to our initial path
+    echo.
+    echo. 
     
     
 ) ELSE (
     echo.
+    echo *
+    echo *                   
+    echo *     ERROR !!!     
+    echo *                   
+    echo *
+    echo *
+    echo *     Provided path not found. Check if path exists and try again ;
+    echo *
+    echo *
     echo.
-    echo ***** ERROR !!! 
-    echo.
-    echo Provided path not found. Check if path exists and try again ;
 )
-GOTO:EOF
-ENDLOCAL
+
+:END_SCRIPT
+    ENDLOCAL
+    echo ~   ENDING SCRIPT
