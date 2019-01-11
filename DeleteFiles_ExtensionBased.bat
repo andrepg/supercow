1dae6009aa294b844f429fd3a674f410d4ec568a
André Paul Grandsire <andrepg@users.noreply.github.com>
2019-01-11 13:22:09 -0200

    Updating file content
    Adding some comments and reorganizing order of commands. We also start the place of a header

--- DeleteFiles_ExtensionBased.bat
+++ DeleteFiles_ExtensionBased.bat
@@ -1,22 +1,25 @@
 @ECHO OFF
 CLS
-:: Setting variables to be local only and get input from user (for now accepting just one extension per running)
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+
+:: First, we set some variables, parameters for input, collation and some characters. 
+:: Then ask user for some input (such as path to search and extension desired). If user don't
+:: provide one of these parameters, script will fail indicating what is missing and forcing
+:: user to run the script again and provide new informations
 SETLOCAL EnableExtensions EnableDelayedExpansion
 chcp 65001 1>nul
-
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-
 SET /p SearchPath=Type path for search..: 
-SET /p ExtensionToSearch=Type the extension that you're searching to [just extension, without dot and asterisk]..: 
+SET /p ExtensionToSearch=Type the extension that you're searching to [just extension, without dot and/or asterisk]..: 
 
 IF [%SearchPath%]==[] (
     echo.
@@ -48,17 +51,19 @@ IF [%ExtensionToSearch%]==[] (
     GOTO:END_SCRIPT
 )
 
+
 IF EXIST %SearchPath% (
     echo.
-    echo ~ Changing to %SearchPath% and searching for *.%ExtensionToSearch%
-    cd /d "%SearchPath%"
-
+    echo ~ OK. We'll search for files with %ExtensionToSearch% extension into 
+    echo     %SearchPath% and his subfolders. 
     echo.
     echo ~ Searching files and saving result in a temporary file
     echo.
     
+    :: If no files was found with DIR command, then ERRORLEVEL will be not equal 0
+    :: by a default system behavior. We check this error level and the script will fail
+    :: when we can find any file into given folder/subfolders with given extension
     dir /s /b %SearchPath%\*.%ExtensionToSearch% 1>"%~dp0temporary_file" 2>nul
-
     IF !ERRORLEVEL! NEQ 0 (
         echo.
         echo *
@@ -78,10 +83,14 @@ IF EXIST %SearchPath% (
     echo ~ Retrieving results and starting to remove files ...
     echo.
 
+    :: For each file saved into temporary_file we'll run the DEL command and 
+    :: provide an output to console telling what file was deleted
     FOR /F "usebackq tokens=* eol=#" %%G in ("%~dp0temporary_file") do ( 
         echo        Deleting file ~^> %%G        
         
         del /f "%%G"
+
+        :: Increase counter of deleted files to show after the script execution
         SET /A "DeletedFilesCounter+=1"
     )
 
