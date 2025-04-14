85fd482c188d5490cde0072da4520e718bed83a7
André Paul Grandsire <andrepg@users.noreply.github.com>
2025-04-13 21:34:56 -0300

    create a backup on update_dolibarr_installation.sh

--- update_dolibarr_installation.sh
+++ update_dolibarr_installation.sh
@@ -4,7 +4,7 @@
 [[ -z "$1" ]] && { echo "Error: Version not specified"; exit 1; }
 
 DOL_URL=https://github.com/Dolibarr/dolibarr/archive/refs/tags/$1.zip
-PUBLIC_PATH="/home/startap/web/manager.startap.dev.br/public_html/"
+PUBLIC_PATH="/var/www/manager.startap.dev.br/"
 
 echo ">> Downloading Dolibarr version $1"
 wget --show-progress $DOL_URL
@@ -12,6 +12,10 @@ wget --show-progress $DOL_URL
 echo ">> Unzipping dolibarr files"
 unzip -q $1.zip
 cd dolibarr-$1/htdocs
+
+echo "Creating documents backup"
+mkdir -p ~/dollibar_bkp
+rsync --human-readable -azup $PUBLIC_PATH ~/dollibar_bkp
 
 echo "Syncing files with Dolibarr instance"
 rsync --human-readable -azuP ./* $PUBLIC_PATH
